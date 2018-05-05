2d9b6443f4dff99e06a132b427e3fd6fb5a3c70c
san1j <sanjanasreenath96@gmail.com>
2018-05-05 16:19:34 -0400

    added a function to calculate GC content

--- src/dnaInfo/DNA_profile.java
+++ src/dnaInfo/DNA_profile.java
@@ -1,5 +1,20 @@
 package dnaInfo;
 
 public class DNA_profile {
-
+	private String dnaString;
+	
+        public DNA_profile(String dnaString){
+        	this.dnaString= dnaString.toUpperCase();
+        }
+        
+        //a simple function to calculate the GC content of the DNA string
+        public int calculateGC(){
+        	int count =0;
+        	if(dnaString.length() == 0) return 0;
+        	for(int i=0; i< dnaString.length(); i++){
+        		if (dnaString.charAt(i) == 'G'|| dnaString.charAt(i) == 'C')
+        			count++;
+        	}
+  
+       }
 }
