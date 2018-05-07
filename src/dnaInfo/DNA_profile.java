65f48af4040485d3794b56d392097df4b076f96f
san1j <sanjanasreenath96@gmail.com>
2018-05-06 20:23:02 -0400

    function to print all the codons in the string

--- src/dnaInfo/DNA_profile.java
+++ src/dnaInfo/DNA_profile.java
@@ -32,8 +32,18 @@ public class DNA_profile {
         	return complement.toString();
         }
         
-        
-        
+        //print all the codons (triplets)
+        public void printCodons(){
+        	//return if string isn't a multiple of 3
+        	if(dnaString.length() % 3 !=0){
+    			System.out.println("Please provide a valid string");
+    			return;
+    		}
+        	//iterate through 1/3 the length of the string and print 3 bases at a time
+        	for(int i = 0; i< dnaString.length()/3; i++){
+        		System.out.println("Codon "+i+ ": "+ dnaString.substring(3*i, 3*i+3) );
+        	}
+        }
         
         
         
